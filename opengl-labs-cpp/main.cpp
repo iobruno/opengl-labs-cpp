77774155b12b2fa1bfdf7b65557d889d5209b049
Bruno <bruno.ariev@gmail.com>
2019-09-17 20:13:18 -0300

    Incl. bootstrap setup for XCode

---
+++ opengl-labs-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opengl-labs-cpp
+//
+//  Created by Bruno on 17/09/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
