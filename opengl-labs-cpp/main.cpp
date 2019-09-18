2db610dd83078840a8dad03e98d5df564424d392
Bruno <bruno.ariev@gmail.com>
2019-09-17 21:59:35 -0300

    Playing around with cam and cursos callback

--- opengl-labs-cpp/main.cpp
+++ opengl-labs-cpp/main.cpp
@@ -1,16 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <GL/glew.h>
-
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include "shader.hpp"
+
+GLFWwindow* window;
+
 using namespace glm;
 
-#include "shader.hpp"
+float zAxis = 0.0;
+float xCam = 0.0;
+float yCam = 0.0;
+
+double mouseInitialPosX;
+double mouseInitialPosY;
+
+
+void scrollCallBack(GLFWwindow *window, double xOffset, double yOffset)
+{
+    if (yOffset > 0) {
+        zAxis += 0.05;
+    } else {
+        zAxis -= 0.05;
+    }
+}
+
+void mouseCallback(GLFWwindow *window, double xOffset, double yOffset)
+{
+    double deslocamentoX = xOffset - mouseInitialPosX;
+    mouseInitialPosX = xOffset;
+
+    double deslocamentoY = yOffset - mouseInitialPosY;
+    mouseInitialPosY = yOffset;
+    
+    xCam += (deslocamentoX * 0.05);
+    yCam += (deslocamentoY * 0.05);
+}
+
 
 int main( void )
 {
@@ -34,6 +62,15 @@ int main( void )
         glfwTerminate();
         return -1;
     }
+    
+    glfwMakeContextCurrent(window);
+    glfwGetCursorPos(window, &mouseInitialPosX, &mouseInitialPosY); // Me da a posicao initial do mouse na tela
+
+    // Scroll callback
+    glfwSetScrollCallback(window, scrollCallBack);
+    // Cursor callback
+    glfwSetCursorPosCallback(window, mouseCallback);
+    
     glfwMakeContextCurrent(window);
     
     glewExperimental = true;
@@ -45,14 +82,16 @@ int main( void )
     }
     
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-    
     GLuint VertexArrayID;
+    
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
     
-    GLuint programID = LoadShaders( "SimpleTransform.vertexshader", "SingleColor.fragmentshader" );
+    GLuint programID = LoadShaders(
+       "SimpleTransform.vertexshader",
+       "SingleColor.fragmentshader"
+   );
     
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
     
@@ -62,15 +101,18 @@ int main( void )
     //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
     
     // Camera matrix
-    glm::mat4 View       = glm::lookAt(
-                                       glm::vec3(4,3,3), // Camera est· em (4,3,3), no World Space
-                                       glm::vec3(0,0,0), // e olhando para a origem
-                                       glm::vec3(0,1,0)  // Est· virada para cima (mude para 0,-1,0 para olhar de cabeÁa para baixo)
-                                       );
+    glm::mat4 View = glm::lookAt(
+       glm::vec3(0,0,3), // Camera est· em (4,3,3), no World Space
+       glm::vec3(0,0,0), // e olhando para a origem
+       glm::vec3(0,1,0)  // Est· virada para cima (mude para 0,-1,0 para olhar de cabeÁa para baixo)
+    );
+    
     // Model matrix : uma matriz identidade (o modelo esta· na origem)
-    glm::mat4 Model      = glm::mat4(1.0f);
+    glm::mat4 Model = glm::mat4(1.0f);
     // Nossa ModelViewProjection : multiplicaÁ„o de nossas 3 matrizes
-    glm::mat4 MVP        = Projection * View * Model; // Lembrem-se, a multiplicaÁ„o de matrizes È inversa
+
+    
+    glm::mat4 MVP = Projection * View * Model; // Lembrem-se, a multiplicaÁ„o de matrizes È inversa
     
     static const GLfloat g_vertex_buffer_data[] = {
         -1.0f, -1.0f, 0.0f,
@@ -83,19 +125,48 @@ int main( void )
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
     
+    float modelXAxis = 0.0;
+    float modelYAxis = 0.0;
+    
     do{
         
         glClear( GL_COLOR_BUFFER_BIT );
-        
         glUseProgram(programID);
         
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            
+            modelXAxis += 0.05;
+        }
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            modelXAxis -= 0.05;
+
+        }
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+            modelYAxis += 0.05;
+        }
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+            modelYAxis -= 0.05;
+
+        }
+
         
+        
+        // Trasladar no eixo Z
+        Model = glm::translate(glm::mat4(1.0f), glm::vec3(modelXAxis, modelYAxis, zAxis));
+        
+        View = glm::lookAt(
+           glm::vec3(xCam, yCam, 3),
+           glm::vec3(0,0,0),
+           glm::vec3(0,1,0)
+        );
+        
+        MVP = Projection * View * Model;
+    
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glVertexAttribPointer(
-                              0,
-                              3,
+                              0,3,
                               GL_FLOAT,
                               GL_FALSE,
                               0,
