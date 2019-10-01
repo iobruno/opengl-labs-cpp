3c6827e6bae9a468ce07ab2b065ca8f49c73b46b
Bruno <bruno.ariev@gmail.com>
2019-10-01 20:59:16 -0300

    Including exemplo 6 (Cube)

--- opengl-labs-cpp/main.cpp
+++ opengl-labs-cpp/main.cpp
@@ -1,176 +1,244 @@
-#include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include "shader.hpp"
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
 GLFWwindow* window;
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-float zAxis = 0.0;
-float xCam = 0.0;
-float yCam = 0.0;
-
-double mouseInitialPosX;
-double mouseInitialPosY;
+#include "shader.hpp"
 
 
-void scrollCallBack(GLFWwindow *window, double xOffset, double yOffset)
-{
-    if (yOffset > 0) {
-        zAxis += 0.05;
-    } else {
-        zAxis -= 0.05;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+void processInput(GLFWwindow *window) {
+    float cameraSpeed = 0.05f;
+    
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     }
 }
 
-void mouseCallback(GLFWwindow *window, double xOffset, double yOffset)
+int main(void)
 {
-    double deslocamentoX = xOffset - mouseInitialPosX;
-    mouseInitialPosX = xOffset;
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Falha ao iniciar o GLFW\n");
+		getchar();
+		return -1;
+	}
 
-    double deslocamentoY = yOffset - mouseInitialPosY;
-    mouseInitialPosY = yOffset;
-    
-    xCam += (deslocamentoX * 0.05);
-    yCam += (deslocamentoY * 0.05);
-}
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	window = glfwCreateWindow(1024, 768, "Exemplo 6", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Falha ao abrir a janela GLFW.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
 
-int main( void )
-{
-    if( !glfwInit() )
-    {
-        fprintf(stderr, "Falha ao iniciar o GLFW\n");
-        getchar();
-        return -1;
-    }
-    
-    glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
-    window = glfwCreateWindow( 1024, 768, "Hello World", NULL, NULL);
-    if( window == NULL ){
-        fprintf(stderr, "Falha ao abrir a janela GLFW.\n");
-        getchar();
-        glfwTerminate();
-        return -1;
-    }
-    
-    glfwMakeContextCurrent(window);
-    glfwGetCursorPos(window, &mouseInitialPosX, &mouseInitialPosY); // Me da a posicao initial do mouse na tela
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Falha ao incializar a GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
 
-    // Scroll callback
-    glfwSetScrollCallback(window, scrollCallBack);
-    // Cursor callback
-    glfwSetCursorPosCallback(window, mouseCallback);
-    
-    glfwMakeContextCurrent(window);
-    
-    glewExperimental = true;
-    if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Falha ao incializar a GLEW\n");
-        getchar();
-        glfwTerminate();
-        return -1;
-    }
-    
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-    GLuint VertexArrayID;
-    
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-    
-    GLuint programID = LoadShaders(
-       "SimpleTransform.vertexshader",
-       "SingleColor.fragmentshader"
-   );
-    
-    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-    
-    // Projection matrix : 45∞ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    mat4 Projection = perspective(radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-    // Ortho camera :
-    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader");
+
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Ortho camera :
+	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
     
     // Camera matrix
-    mat4 View = lookAt(
-       vec3(0,0,3), // Camera est· em (4,3,3), no World Space
-       vec3(0,0,0), // e olhando para a origem
-       vec3(0,1,0)  // Est· virada para cima (mude para 0,-1,0 para olhar de cabeÁa para baixo)
-    );
-    
-    // Model matrix : uma matriz identidade (o modelo esta· na origem)
-    mat4 Model = mat4(1.0f);
-    // Nossa ModelViewProjection : multiplicaÁ„o de nossas 3 matrizes
-    
-    mat4 MVP = Projection * View * Model; // Lembrem-se, a multiplicaÁ„o de matrizes È inversa
-    
-    static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f,  1.0f, 0.0f,
-    };
-    
-    GLuint vertexbuffer;
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-    
-    float modelXAxis = 0.0;
-    float modelYAxis = 0.0;
-    
-    do{
-        glClear( GL_COLOR_BUFFER_BIT );
-        glUseProgram(programID);
-        
-        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-            modelXAxis += 0.05;
-        }
-        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-            modelXAxis -= 0.05;
-        }
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-            modelYAxis += 0.05;
-        }
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-            modelYAxis -= 0.05;
+	glm::mat4 View = glm::lookAt(cameraPos, cameraPos + cameraFront , cameraUp);
 
-        }
+    
+    // Model matrix : uma matriz identidade (o modelo esta� na origem)
+	glm::mat4 Model = glm::mat4(1.0f);
+	// Nossa ModelViewProjection : multiplica��o de nossas 3 matrizes
+	glm::mat4 MVP = Projection * View * Model; // Lembrem-se, a multiplica��o de matrizes � inversa
 
+	// Os vértices. Três floats consecutivos formam um vértice 3D; 
+	// Três vértices consecutivos formam um triângulo.
+	// Um cubo tem 6 faces com 2 triângulos cada, então são 6*2=12 triângulos e 12*3 vértices
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f,-1.0f,-1.0f, // triângulo 1 : inicio
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, // triângulo 1 : fim
+		1.0f, 1.0f,-1.0f, // triângulo 2 : inicio
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f, // triângulo 2 : fim
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
+	};
+
+	// Uma cor RGB aleatória para cada vétice. 
+	static const GLfloat g_color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+	do {
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(programID);
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
+		// 2o buffer de atributo : cores
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+			1,                                
+			3,                                
+			GL_FLOAT,                         
+			GL_FALSE,                         
+			0,                                
+			(void*)0 
+		);
         
-        // Trasladar no eixo Z
-        Model = translate(mat4(1.0f), vec3(modelXAxis, modelYAxis, zAxis));
-        View = lookAt(vec3(xCam, yCam, 3), vec3(0,0,0), vec3(0,1,0));
+        processInput(window);
+        
+        View = glm::lookAt(cameraPos, cameraPos + cameraFront , cameraUp);
+        
         MVP = Projection * View * Model;
-    
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-        
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDisableVertexAttribArray(0);
-        
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        
-    }
-    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-          glfwWindowShouldClose(window) == 0 );
-    
-    glDeleteBuffers(1, &vertexbuffer);
-    glDeleteProgram(programID);
-    glDeleteVertexArrays(1, &VertexArrayID);
-    
-    glfwTerminate();
-    
-    return 0;
-}
 
+		// Desenha o triângulo
+		glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 índices começando em 0 -> 12 triângulos -> 6 quadrados
+
+		glDisableVertexAttribArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colorbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	glfwTerminate();
+
+	return 0;
+}
