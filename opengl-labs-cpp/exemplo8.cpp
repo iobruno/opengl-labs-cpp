4294e939ba1a6ee54d236e9c65fee0fdab435747
Bruno <bruno.ariev@gmail.com>
2019-10-29 21:50:00 -0300

    including 2nd object for translation

--- opengl-labs-cpp/exemplo8.cpp
+++ opengl-labs-cpp/exemplo8.cpp
@@ -75,13 +75,19 @@ int main(void)
 	);
 	// Model matrix : uma matriz identidade (o modelo está na origem)
 	glm::mat4 Model = glm::mat4(1.0f);
+    Model = glm::scale(Model, vec3(0.3f));
+    
 	// Nossa ModelViewProjection : multiplicação de nossas 3 matrizes
-    
-    // Transladar -> Rodar
-    glm::translate(Model, glm::vec3(3.0f, 0.0f, 0.0f));
-    
 	glm::mat4 MVP = Projection * View * Model; // Lembrem-se, a multiplicação de matrizes é inversa
 
+
+    // Another Model
+    glm::mat4 anotherModel = glm::mat4(1.0f);
+    anotherModel = glm::translate(anotherModel, vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 anotherMVP = Projection * View * anotherModel;
+    
+
+    
 	// Carrega e cria uma textura
 	// -------------------------
 	GLuint Texture;
@@ -96,6 +102,7 @@ int main(void)
 	// carrega a imagem, cria textura e gera o mipmaps
 	int width, height, nrChannels;
 	unsigned char* data = stbi_load("uvtemplate.bmp", &width, &height, &nrChannels, 0);
+    
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -109,54 +116,90 @@ int main(void)
 
 	GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
-	// Read our .obj file
+	// FIRST OBJECT - CUBO
 	std::vector<glm::vec3> vertices;
 	std::vector<glm::vec2> uvs;
 	std::vector<glm::vec3> normais; // Não está sendo usado
-	bool res = loadOBJ("cruiser.obj", vertices, uvs, normais);
+	bool res = loadOBJ("esfera.obj", vertices, uvs, normais);
+    
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    
+    GLuint uvbuffer;
+    glGenBuffers(1, &uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
+    
+    
+    // SECOND OBJECT - SPACESHIP
+    std::vector<glm::vec3> anotherVertices;
+    std::vector<glm::vec2> anotherUvs;
+    std::vector<glm::vec3> anotherNormals;
+    bool anotherRes = loadOBJ("cruiser.obj", anotherVertices, anotherUvs, anotherNormals);
 
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+    GLuint anotherVertexbuffer;
+    glGenBuffers(1, &anotherVertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, anotherVertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, anotherVertices.size() * sizeof(glm::vec3), &anotherVertices[0], GL_STATIC_DRAW);
+    
+    GLuint anotherUvbuffer;
+    glGenBuffers(1, &anotherUvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, anotherUvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, anotherUvs.size() * sizeof(glm::vec2), &anotherUvs[0], GL_STATIC_DRAW);
 
-	GLuint uvbuffer;
-	glGenBuffers(1, &uvbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
-
+    float counter = 0.0f;
+    
 	do {
-
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		glUseProgram(programID);
 
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        // FIRST OBJECT
+        Model = glm::rotate(Model, 0.001f, vec3(0.0f, 1.0f, 0.0f));
+        MVP = Projection * View * Model;
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
-		glUniform1i(TextureID, 0);
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        glActiveTexture(GL_TEXTURE0);
+        
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        glUniform1i(TextureID, 0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
+        
+        // SECOND OBJECT
+        anotherModel = glm::translate(glm::mat4(1.0f), vec3(3.0f * sin(counter), 0.0f, 3.0f * cos(counter)));
+        anotherMVP = Projection * View * anotherModel;
+        
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &anotherMVP[0][0]);
+        glActiveTexture(GL_TEXTURE0);
+        
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        glUniform1i(TextureID, 0);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, anotherVertexbuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, anotherUvbuffer);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, anotherVertices.size());
 
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-		// Desenha o objeto
-		glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
-
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+        counter = counter + 0.01f;
 
-	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-		glfwWindowShouldClose(window) == 0);
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
